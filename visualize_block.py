c08fc3c55b7f88e76c90765d70ecbcb2e2490682
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-10-22 00:29:17 -0400

    Render number while visualizing block

--- visualize_block.py
+++ visualize_block.py
@@ -13,6 +13,9 @@ SIM_STEPS = 32
 
 block = spacetime_block(steps=SIM_STEPS, factor=4, height=512, width=512, batch_size=1)
 
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 while running and i <= SIM_STEPS * 4:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # user closed window
@@ -20,6 +23,9 @@ while running and i <= SIM_STEPS * 4:
 
     screen.fill("black")
     draw_matrix(block[i][0].tolist(), screen, 5)
+
+    text_surface = my_font.render(str(i/4), False, (255, 255, 255))
+    screen.blit(text_surface, (0, 0))
 
     pygame.display.flip()
     i = i+1
