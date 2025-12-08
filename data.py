c8bd41f48d011e9b1b04afe23bbbc8b938561d21
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-12-07 23:58:32 -0500

    Fix data generation

--- data.py
+++ data.py
@@ -2,14 +2,16 @@ import torch, os
 from utils import spacetime_block
 import constants
 
-num_files = 5
+num_files = 2
+directory = "data/valid"
+
 size = 1024
 steps = 32
 
 start = 0
-while os.path.isfile("data/universe" + str(start)):
+while os.path.isfile(directory + "/" + str(start) + ".pt"):
     start += 1
 
 for i in range(start, start + num_files):
     universe = spacetime_block(steps, constants.SCALE, size, size)
-    torch.save(universe, "data/universe" + str(i))
+    torch.save(universe, directory + str(i) + ".pt")
