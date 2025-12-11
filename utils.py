7aa9f3ea0459681d904ec117a726103ca904927d
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-12-10 20:24:51 -0500

    More memory efficient generation

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def downscale(tensor, factor):
 
 def trimmed_spacetime_block(steps, factor, height, width, batch_size=1):
     out = spacetime_block(steps, factor, height, width, batch_size, time_factor=factor+1)
-    mask = torch.arange(out.shape[1]) % factor != 0
+    mask = torch.arange(out.shape[1]) % (factor + 1) != 0
     out = out[:, mask]
     return out
 
@@ -26,27 +26,28 @@ def spacetime_block(steps, factor, height, width, batch_size=1, time_factor=None
     # assert batch_size == 1, "batch size not implemented for any number other than 1"
     if height % factor != 0 or width % factor != 0:
         raise ValueError("height and width dimensions must be divisible by factor")
-    if time_factor == None: 
-        time_factor = factor
-    probability = random.triangular(0, 0.6, 0.3)
+    
+    probability = random.triangular(0.1, 0.6, 0.3)
 
     # Generate initial state
-    states = torch.bernoulli(
+    states = torch.empty(batch_size, steps + 1, int(height/4), int(width/4))
+    states[:, 0] = torch.bernoulli(
         input=torch.full(
-            size=(batch_size, 1, int(height/4), int(width/4)),
+            size=(batch_size, int(height/4), int(width/4)),
             fill_value=probability
         )
     )
     # (B, 1, H/4, W/4)
-    
-    for t in tqdm(range(steps), desc=f"Generating Data"): 
-        new_state = torch.empty(batch_size, 1, int(height/4), int(width/4))
-        for i in range(batch_size): 
-            new_state[i][0] = update_game(states[i][t])
-        states = torch.cat((states, new_state), dim=1)
 
+    for t in tqdm(range(steps), desc=f"Generating Data"): 
+        states[:, t + 1] = states[:, t]
+        for b in range(batch_size):
+            states[b][t + 1] = update_game(states[b][t])
+
+    if time_factor == None: 
+        time_factor = factor
     states = upscale(states, time_factor, factor)
     # states = states.permute(1, 0, 2, 3) # spacetime block (B=1, (steps+T+1) * 4 , H, W)
     return states
 
-# print(spacetime_block(8, 4, 128, 128, batch_size=2).shape)
+print(spacetime_block(8, 4, 128, 128, batch_size=2).shape)
