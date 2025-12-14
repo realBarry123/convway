62e7ee576138d681b6f40eb46411bf13610ef20f
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-12-14 13:43:24 -0500

    Make model Markov (3M)

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from constants import *
 
 B = 32
 
-def get_conv_stack(channels: list[int], kernel_size=SCALE*3, do_relu=True) -> list[Callable]:
+def get_conv_stack(channels: list[int], kernel_size, do_relu=True) -> list[Callable]:
     layers = []
     for i in range(len(channels) - 1):
         layers.append(nn.Conv2d(
@@ -19,58 +19,34 @@ def get_conv_stack(channels: list[int], kernel_size=SCALE*3, do_relu=True) -> li
 
     return layers
 
-def get_smusher(mode: str):
-    VALID_MODES = ["last", "4th", "mean", "conv"]
-    if mode not in VALID_MODES:
-            raise ValueError(f"invalid smushing mode: expected one of [\"{"\", \"".join(VALID_MODES)}\"] but got \"{squeeze_mode}\"")
-    if mode == "last":
-        return lambda x: x[:, SCALE-1:, :, :]
-    if mode == "4th":
-        return lambda x: x[:, :1, :, :]
-    if mode == "mean":
-        return lambda x: torch.mean(x, dim=1, keepdim=True)
-    if mode == "conv": 
-        return torch.nn.Conv2d(
-            in_channels=4, 
-            out_channels=1, 
-            kernel_size=(SCALE * 3, SCALE * 3), 
-            padding="same"
-        )
-
 
 class ConvwayNet(torch.nn.Module):
 
-    def __init__(self, x_smush_mode: str, r_smush_mode: str, conv_channels: list[int], do_relu: bool):
+    def __init__(self, conv_channels: list[int], do_relu):
         super().__init__()
 
+        assert conv_channels[0] == 1
+        assert conv_channels[-1] == 1
+
         self.configs = {
-            "x_smush_mode": x_smush_mode,
-            "r_smush_mode": r_smush_mode, 
             "conv_channels": conv_channels,
             "do_relu": do_relu
         }
-        
-        # Downscaling (T) layer 
-        self.smush_x = get_smusher(x_smush_mode)
-        self.smush_r = get_smusher(r_smush_mode)
 
         # Conv layers
-        self.convs = nn.Sequential(*get_conv_stack(conv_channels, do_relu=do_relu))
+        self.convs = nn.Sequential(*get_conv_stack(conv_channels, kernel_size=SCALE*3, do_relu=do_relu))
 
         # Activations
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x: torch.Tensor):  # (B, T=4, H, W)
-        r = self.smush_r(x)  # (B, T=1, H, W)
+    def forward(self, x: torch.Tensor):  # (B, C=1, H, W)
 
-        r = self.convs(r)  # (B, T=1, H, W)
-
-        x = self.smush_x(x)
+        r = self.convs(x)
         x = self.sigmoid(x + r)
         
         return x
 
 # Test code does not run on import
 if __name__ == "__main__":
-    model = ConvwayNet(x_smush_mode="last", r_smush_mode="conv", conv_channels=[1, 4, 1], do_relu=True)
-    summary(model, input_size=(SCALE, 69, 420), batch_size=B)
+    model = ConvwayNet(conv_channels=[1, 4, 1], do_relu=True)
+    summary(model, input_size=(1, 69, 420), batch_size=B)
