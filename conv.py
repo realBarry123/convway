a49a7588ad8e92431e744654b9e9542de9d838cb
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-12-27 09:22:58 +0800

    Multiple in/out channel support

--- conv.py
+++ conv.py
@@ -5,30 +5,32 @@ import torch.nn.functional as F
 
 class SymConv2d(nn.Module):
 
-    def __init__(self, kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True):
         super().__init__()
+        self.in_c = in_channels
+        self.out_c = out_channels
         self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.doBias = bias
         self.param_width = math.ceil(kernel_size/2)
-        self.params = nn.Parameter(data=torch.full(((self.param_width**2 + self.param_width)//2,), 3.0))
+        self.params = nn.Parameter(data=torch.randn((self.out_c, self.in_c, (self.param_width**2 + self.param_width)//2)))
 
     def forward(self, x):
-        kernel = torch.empty((self.kernel_size, self.kernel_size))
-        indices = torch.triu_indices(self.param_width, self.param_width)
-        kernel[indices[0], indices[1]] = self.params
-        kernel[indices[0], -indices[1]] = self.params
-        kernel[-indices[0], indices[1]] = self.params
-        kernel[-indices[0], -indices[1]] = self.params
-        indices[0], indices[1] = indices[1], indices[0]
-        kernel[indices[0], indices[1]] = self.params
-        kernel[indices[0], -indices[1]] = self.params
-        kernel[-indices[0], indices[1]] = self.params
-        kernel[-indices[0], -indices[1]] = self.params
-        kernel = kernel.unsqueeze(0).unsqueeze(0)
-        return F.conv2d(x, kernel, padding="same")
+        kernel = torch.zeros((self.out_c, self.in_c, self.kernel_size, self.kernel_size))
+        row, col = torch.triu_indices(self.param_width, self.param_width)
+
+        for i in range(4):
+            kernel[:, :, row, col] = self.params
+            row, col = col, row # diagonal reflection
+
+            kernel[:, :, row, col] = self.params
+            row = self.kernel_size-row-1 # paragonal reflection
+
+        return F.conv2d(x, kernel, stride=self.stride, padding=self.padding)
 
 
 if __name__ == "__main__":
-    x = torch.full((1, 1, 9, 9), 2.0)
-    conv = SymConv2d(1)
+    x = torch.full((1, 2, 9, 9), 2.0)
+    conv = SymConv2d(in_channels=2, out_channels=2, kernel_size=5, padding="same")
     x = conv(x)
-    print(x)
